fac0fd90c34763c1aff9cee9d5e3b95dd651bbdd
Pedro Torres <pedrolucast.2727@gmail.com>
2024-07-22 13:28:54 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projeto_site_react"
-})
+  base: "/projeto_login_react",
+});
